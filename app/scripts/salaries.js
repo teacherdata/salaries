5e64938bf9538257cb7b878a4777a241057a4b5d
Chris Vogt <mail@chrisvogt.me>
2017-05-01 00:30:09 -0700

    Update styles and begin to pull out a lot of chrisvogt.me content.

--- app/scripts/salaries.js
+++ app/scripts/salaries.js
@@ -1,130 +1,132 @@
-/**
- * Salaries module
- *
- * Retrieves and formats teacher salary data
- *
- * @author Chris Vogt <mail@chrisvogt.me>
- */
-var salaries = (function(document, $) {
-
-  'use strict';
-
+$(function() {
   /**
-   * Data endpoint.
+   * Salaries module
    *
-   * @var {String}
+   * Retrieves and formats teacher salary data
+   *
+   * @author Chris Vogt <mail@chrisvogt.me>
    */
-  var endpoint = 'https://cdn.rawgit.com/chrisvogt/ed85273a674001e3c585/raw/d8db84d982d666dc9b5a18a5c60c6f8756818181/teacher-est-avg-salary.json';
+  var salaries = (function(document, $) {
 
-  /**
-   * Init method.
-   */
-  function init() {
-    applyUIBindings();
-    getData();
-  }
+    'use strict';
 
-  /**
-   * Get data and start the process.
-   */
-  function getData() {
-    return $.ajax({
-      url: endpoint,
-      dataType: 'json',
-      success: function(r) {
-        stateFactory(r);
+    /**
+     * Data endpoint.
+     *
+     * @var {String}
+     */
+    var endpoint = 'https://cdn.rawgit.com/chrisvogt/ed85273a674001e3c585/raw/d8db84d982d666dc9b5a18a5c60c6f8756818181/teacher-est-avg-salary.json';
+
+    /**
+     * Init method.
+     */
+    function init() {
+      applyUIBindings();
+      getData();
+    }
+
+    /**
+     * Get data and start the process.
+     */
+    function getData() {
+      return $.ajax({
+        url: endpoint,
+        dataType: 'json',
+        success: function(r) {
+          stateFactory(r);
+        }
+      }).responseJSON;
+    }
+
+    /**
+     * Create states.
+     */
+    function stateFactory(data) {
+      var states = extractStates(data);
+
+      buildStateOptions(states);
+      setStates([states[Math.floor(Math.random() * states.length) + 1], states[Math.floor(Math.random() * states.length) + 1]], data);
+    }
+
+    /**
+     * Extracts an array of states from the states object.
+     */
+    function extractStates(data) {
+      var states = [];
+
+      for (var state in data) {
+        if (data.hasOwnProperty(state) && state !== 'United States') {
+          states.push(state);
+        }
       }
-    }).responseJSON;
-  }
 
-  /**
-   * Create states.
-   */
-  function stateFactory(data) {
-    var states = extractStates(data);
+      return states;
+    }
 
-    buildStateOptions(states);
-    setStates([states[Math.floor(Math.random() * states.length) + 1], states[Math.floor(Math.random() * states.length) + 1]], data);
-  }
+    /**
+     * Builds state options for form select inputs.
+     */
+    function buildStateOptions(states) {
+      var el = $('.states');
 
-  /**
-   * Extracts an array of states from the states object.
-   */
-  function extractStates(data) {
-    var states = [];
-
-    for (var state in data) {
-      if (data.hasOwnProperty(state) && state !== 'United States') {
-        states.push(state);
+      for (var state in states) {
+        el.append('<option>' + states[state] + '</option>');
       }
     }
 
-    return states;
-  }
+    /**
+     * Loads state data into the chart series.
+     */
+    function setStates(states, data) {
+      var chart = $('#container').highcharts();
 
-  /**
-   * Builds state options for form select inputs.
-   */
-  function buildStateOptions(states) {
-    var el = $('.states');
-
-    for (var state in states) {
-      el.append('<option>' + states[state] + '</option>');
+      for (var i = 0; i < 2; i++) {
+        chart.legend.allItems[i].update({
+          name: states[i]
+        });
+      }
+      chart.series[0].setData(getValues(data[states[0]]), false);
+      chart.series[1].setData(getValues(data[states[1]]), false);
+      chart.redraw();
     }
-  }
 
-  /**
-   * Loads state data into the chart series.
-   */
-  function setStates(states, data) {
-    var chart = $('#container').highcharts();
+    /**
+     * Extracts values from a state set.
+     */
+    function getValues(series) {
+      var values = [];
 
-    for (var i = 0; i < 2; i++) {
-      chart.legend.allItems[i].update({
-        name: states[i]
+      for (var key in series) {
+        if (series.hasOwnProperty(key)) {
+          values.push(series[key]);
+        }
+      }
+
+      return values;
+    }
+
+    /**
+     * Apply UI bindings.
+     */
+    function applyUIBindings() {
+      /* Select box events */
+      $('.states').change(function() {
+        if ($('#stateA').val() !== 'State A' && $('#stateB').val() !== 'State B') {
+          return $.ajax({
+            url: endpoint,
+            dataType: 'json',
+            success: function(r) {
+              stateFactory(r);
+              setStates([$('#stateA').val(), $('#stateB').val()], r);
+            }
+          }).responseJSON;
+        }
       });
     }
-    chart.series[0].setData(getValues(data[states[0]]), false);
-    chart.series[1].setData(getValues(data[states[1]]), false);
-    chart.redraw();
-  }
 
-  /**
-   * Extracts values from a state set.
-   */
-  function getValues(series) {
-    var values = [];
+    return {
+      init: init
+    };
 
-    for (var key in series) {
-      if (series.hasOwnProperty(key)) {
-        values.push(series[key]);
-      }
-    }
-
-    return values;
-  }
-
-  /**
-   * Apply UI bindings.
-   */
-  function applyUIBindings() {
-    /* Select box events */
-    $('.states').change(function() {
-      if ($('#stateA').val() !== 'State A' && $('#stateB').val() !== 'State B') {
-        return $.ajax({
-          url: endpoint,
-          dataType: 'json',
-          success: function(r) {
-            stateFactory(r);
-            setStates([$('#stateA').val(), $('#stateB').val()], r);
-          }
-        }).responseJSON;
-      }
-    });
-  }
-
-  return {
-    init: init
-  };
-
-})(document, jQuery);
+  })(document, jQuery);
+});
