554b38c0ce8406dbda321f73e6daf538711df431
Chris Vogt <mail@chrisvogt.me>
2017-06-07 21:18:21 -0700

    Update Highcharts version and remove unused dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -105,7 +105,8 @@ gulp.task('serve', ['styles', 'scripts', 'fonts'], () => {
     server: {
       baseDir: ['.tmp', 'app'],
       routes: {
-        '/bower_components': 'bower_components'
+        '/bower_components': 'bower_components',
+        '/node_modules': 'node_modules'
       }
     }
   })
