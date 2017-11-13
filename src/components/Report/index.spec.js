f77c1aeaec75090fd071114b36ba6765d95b67d7
Chris Vogt <mail@chrisvogt.me>
2017-11-12 21:05:08 -0800

    Bump dependencies and improve test coverage.

--- src/components/Report/index.spec.js
+++ src/components/Report/index.spec.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import { stub } from 'sinon'
 import Report from './'
 import Notice from './Notice'
 
@@ -11,4 +12,11 @@ it('properly handles updating to no selected states', () => {
   const wrapper = shallow(<Report />)
   wrapper.setState({ states: [] })
   expect(wrapper.contains(<Notice />)).toEqual(true)
+})
+
+it('is able to properly delete states', () => {
+  const expected = ['CA', 'NY']
+  const wrapper = shallow(<Report />)
+  wrapper.instance().handleDeleteState('AZ')
+  expect(wrapper.state().states).toEqual(expect.arrayContaining(expected))
 })
